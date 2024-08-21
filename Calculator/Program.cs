e6883c00b95759e79e5df4c2e982e6eab64cc775
Soniya <Soniya@example.com>
2024-08-21 15:27:42 +0545

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1;
+            int x2;
+            int result;
+            string compute;
+            Console.WriteLine("Hello welcome to the calculator program");
+            Console.WriteLine("Please enter your first number");
+
+            x1=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number");
+            x2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do");
+            Console.WriteLine("Please enter a for addition,s for substraction,m for multiplication, and d for division");
+
+            compute = Console.ReadLine();
+
+            if(compute == "a")
+            {
+                result=x1 + x2;
+            }
+            else if(compute =="s")
+            {
+                result = x1 - x2;
+            }
+            else if(compute=="m")
+            {
+                result = x1 * x2;
+            }
+           
+            else
+            {
+                result= x1/x2;
+            }
+            Console.WriteLine("The result is "+result);
+            Console.WriteLine("Thankyou for using the calculator program done by Soniya Pantha!");
+            Console.ReadKey();
+
+        }
+    }
+}
